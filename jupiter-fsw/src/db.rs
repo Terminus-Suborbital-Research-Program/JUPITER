0a6e19420894f5a721224f823f797d4c6c955a6b
Lucas Thelen <thelenlucas028@gmail.com>
2025-03-10 22:57:07 -0400

    Packets

---
+++ jupiter-fsw/src/db.rs
@@ -0,0 +1,93 @@
+use std::{sync::{Arc, Mutex}, time::SystemTime};
+
+use bin_packets::{ApplicationPacket, UnixTimestampMillis};
+use bincode::{config::standard, decode_from_slice, encode_to_vec, error::DecodeError};
+use sqlite::Connection;
+
+#[derive(Debug)]
+pub struct CachedPacket {
+    data: Vec<u8>,
+    timestamp: UnixTimestampMillis,
+}
+
+impl CachedPacket {
+    pub fn new(packet: ApplicationPacket) -> Self {
+        Self::from(packet)
+    }
+
+    pub(super) fn raw(packet: &[u8], time: u64) -> Self {
+        CachedPacket {
+            data: Vec::from(packet),
+            timestamp: UnixTimestampMillis::new(time),
+        }
+    }
+
+    pub fn inner(self) -> (Vec<u8>, UnixTimestampMillis) {
+        (self.data, self.timestamp)
+    }
+}
+
+impl From<ApplicationPacket> for CachedPacket {
+    fn from(value: ApplicationPacket) -> Self {
+        let now = SystemTime::now();
+        let timestamp = now.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u64;
+        let timestamp = UnixTimestampMillis::new(timestamp);
+
+        let data = encode_to_vec(value, standard()).unwrap();
+
+        CachedPacket {
+            data,
+            timestamp,
+        }
+    }
+}
+
+impl TryFrom<CachedPacket> for ApplicationPacket {
+    type Error = DecodeError;
+
+    fn try_from(value: CachedPacket) -> Result<Self, Self::Error> {
+        let res: (ApplicationPacket, usize) = decode_from_slice(&value.data, standard())?;
+        Ok(res.0)
+    }
+}
+
+pub struct PacketsCacheHandler {
+    db: Arc<Mutex<Connection>>,
+}
+
+impl PacketsCacheHandler {
+    pub fn new(database: &Arc<Mutex<Connection>>) -> Self {
+        PacketsCacheHandler {
+            db: Arc::clone(database)
+        }
+    }
+
+    pub fn insert_cached_packet(&self, packet: CachedPacket) {
+        let connection = self.db.lock().unwrap();
+        
+        let statement = "INSERT INTO current_powercycle (timestamp, data) VALUES (?, ?);";
+        let mut statement = connection.prepare(statement).unwrap();
+        let (data, time) = packet.inner();
+        statement.bind((1, time.timestamp as i64)).unwrap();
+        statement.bind((2, data.as_slice())).unwrap();
+        statement.iter();
+        for _ in statement.iter() {}
+    }
+
+    pub fn most_recent_packet(&self) -> Option<CachedPacket> {
+        let connection = self.db.lock().unwrap();
+
+        let statement = "SELECT * FROM current_powercycle ORDER BY timestamp DESC;";
+        let mut statement = connection.prepare(statement).unwrap();
+        match statement.next() {
+            Ok(_) => {
+                Some(CachedPacket::raw(
+                    &statement.read::<Vec<u8>, _>("data").unwrap(),
+                    statement.read::<i64, _>("timestamp").unwrap() as u64,
+                ))
+            }
+
+            Err(_) => None,
+        }
+    }
+}
--- jupiter-fsw/src/main.rs
+++ jupiter-fsw/src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, sync::{Arc, Mutex}};
+
+use bin_packets::ApplicationPacket;
+use jupiter_fsw::db::{CachedPacket, PacketsCacheHandler};
+
 fn main() {
+    let packets_db_loc = env::var("PACKETS_DATABASE").expect("Couldn't find packet database location!");
+    let connection = sqlite::open(packets_db_loc).expect("Database open failure!");
+    let connection = Arc::new(Mutex::new(connection));
+    let handler = PacketsCacheHandler::new(&connection);
+
+    let demo_packet = ApplicationPacket::Command(bin_packets::CommandPacket::Ping);
+    let demo_packet = CachedPacket::new(demo_packet);
+    println!("Last packet: {:?}", handler.most_recent_packet());
+    handler.insert_cached_packet(demo_packet);
+
     println!("Hello, world!");
 }
