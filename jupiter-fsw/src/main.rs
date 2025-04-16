b3be990273431643db86d3bbfbbf8c9d5fd30c37
Lucas Thelen <thelenlucas028@gmail.com>
2025-04-15 19:22:23 -0500

    I2C read working!

--- jupiter-fsw/src/main.rs
+++ jupiter-fsw/src/main.rs
@@ -1,13 +1,17 @@
 use std::{
     env::set_var,
+    io::Read,
     sync::{Arc, Mutex},
-    thread::sleep,
+    thread::{self, sleep},
     time::Duration,
 };
 
 use env_logger::Env;
+use i2cdev::{core::I2CDevice, linux::LinuxI2CDevice};
+use palantir::ping_thread;
 
 mod db;
+mod palantir;
 mod tasks;
 
 use crate::db::db_init;
@@ -17,7 +21,13 @@ fn main() {
     let env = Env::default().filter_or("LOG_LEVEL", "info");
     env_logger::init_from_env(env);
 
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", 0x26u16).unwrap();
+
+    info!("I2c Read: {:?}", dev.smbus_read_byte());
+
     db_init();
+
+    thread::spawn(move || ping_thread());
 
     info!("Current Iteration: {}", db::current_iteration_num());
 
